db87d4a1a9fffe31ea0d4f0f4894d9569d28c336
Bruno Ferreira <bruno55ferreira55@gmail.com>
2022-08-22 20:00:16 -0300

    criacao de duas classe que colocam a imagem no fundo da pagina e no box centralizado

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const img = document.getElementById('imagem');
+const container = document.getElementById('backgroundBlur');
+const boxImagem = document.getElementById('box-imagem')
 const button = document.getElementById('botao')
 .addEventListener('click', getImagem);
 
 async function getImagem() {
     console.log('ok')
-
+    
     let dados = await fetch('https://dog.ceo/api/breeds/image/random');
-
+    
     let dadosImagem = await dados.json();
-
-    img.src = dadosImagem.message;
+    
+    backgroundBlur(dadosImagem)
+    showImagem(dadosImagem)
+    
 }
 
+function backgroundBlur(bck) {
+    container.style.backgroundImage = `url(${bck.message})`; 
+}
+
+function showImagem(img) {
+    boxImagem.style.backgroundImage = `url(${img.message})`;
+}
