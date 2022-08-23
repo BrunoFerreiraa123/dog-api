95d857f365980cdb26d3b2dd502aab35c3654efe
Bruno Ferreira <bruno55ferreira55@gmail.com>
2022-08-22 21:19:31 -0300

    criacao de funcao que pega o nome da raça de cada cachorro

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
-const img = document.getElementById('imagem');
 const container = document.getElementById('backgroundBlur');
-const boxImagem = document.getElementById('box-imagem')
-const button = document.getElementById('botao')
+const img = document.getElementById('imagem');
+const boxImagem = document.getElementById('box-image')
+
+const button = document.getElementById('gerateImage')
 .addEventListener('click', getImagem);
 
 async function getImagem() {
-    console.log('ok')
     
     let dados = await fetch('https://dog.ceo/api/breeds/image/random');
     
     let dadosImagem = await dados.json();
-    
-    backgroundBlur(dadosImagem)
-    showImagem(dadosImagem)
+
+    raca(dadosImagem.message)
+    backgroundBlur(dadosImagem.message)
+    showImagem(dadosImagem.message)
     
 }
 
 function backgroundBlur(bck) {
-    container.style.backgroundImage = `url(${bck.message})`; 
+    container.style.backgroundImage = `url(${bck})`; 
 }
 
 function showImagem(img) {
-    boxImagem.style.backgroundImage = `url(${img.message})`;
+    boxImagem.style.backgroundImage = `url(${img})`;
+}
+
+function raca(text) {
+    let textoFormatado = text.substring(30, text.lastIndexOf('/'))
 }
