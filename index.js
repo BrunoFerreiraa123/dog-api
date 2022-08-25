e710075d543bccb0f8eebe92cdece29bd4fc1ade
Bruno Ferreira <bruno55ferreira55@gmail.com>
2022-08-25 20:52:59 -0300

    ao clicar no botao os icones ganham um display inline e o botao display none

--- index.js
+++ index.js
@@ -2,17 +2,31 @@ const container = document.getElementById('background-blur');
 const boxImagem = document.getElementById('image');
 const dogName = document.getElementById('dog-name');
 
-const button = document.getElementById('gerate-image')
-.addEventListener('click', getImagem);
+const button = document.getElementById('gerate-image');
+const previousImg = document.getElementById('previous-image');
+const nextImg = document.getElementById('next-image');
+
+let arrayImg = []
+
+button.addEventListener('click', getImagem);
+nextImg.addEventListener('click', getImagem);
 
 async function getImagem() {
+    button.style.display = "none";
+    previousImg.style.display = "inline";
+    nextImg.style.display = "inline";
+
+    console.log(arrayImg);
+
     let response = await fetch('https://dog.ceo/api/breeds/image/random');
     let jsonDog = await response.json();
     let url = jsonDog.message
        
     backgroundBlur(url)
     showImagem(url)
-    raca(url)
+    race(url)
+
+    return arrayImg.push(url);
 }
 
 function backgroundBlur(bck) {
@@ -24,7 +38,7 @@ function showImagem(img) {
     boxImagem.style.display = 'block';
 }
 
-function raca(text) {
+function race(text) {
     let textoFormatado = text.substring(30, text.lastIndexOf('/'));
     dogName.innerText = `${textoFormatado}  `;
     dogName.href = `https://www.google.com/search?q=dog+${textoFormatado}`
